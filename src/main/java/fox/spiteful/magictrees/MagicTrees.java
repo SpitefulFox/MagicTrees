ab53e77c9a49459fcb99c4cecb8e49598ba82396
Jeremy Diamond <diamondjbj2@yahoo.com>
2015-05-29 15:45:29 -0700

    Update

---
+++ src/main/java/fox/spiteful/magictrees/MagicTrees.java
@@ -0,0 +1,30 @@
+package fox.spiteful.magictrees;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "MagicTrees", name = "Magic Trees", dependencies = "required-after:Forestry;after:Thaumcraft;after:AWWayofTime;after:Botania")
+public class MagicTrees {
+
+    @Instance
+    public static MagicTrees instance;
+
+    @EventHandler
+    public void sapling(FMLPreInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void growth(FMLInitializationEvent event){
+
+    }
+
+    @EventHandler
+    public void chop(FMLPostInitializationEvent event){
+
+    }
+}
